113cc14bcf2d93d8345cfe2d17d6f20e7c44f359
Avinash Singh <avinash.20399@gmail.com>
2021-04-01 17:33:20 +0530

    Initialised app

--- bot-api/app.js
+++ bot-api/app.js
@@ -1,20 +1,21 @@
+//Prebuilt modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// Initiating app
 var app = express();
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Primary Router
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
