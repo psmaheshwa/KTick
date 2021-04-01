937144dc56d778fdc18fa991250533377a8d050f
Maheshwaran P <maheshwaran.cs17@bitsathy.ac.in>
2021-04-01 18:19:53 +0530

    HttpClient

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'KTick';
+  loginForm: FormGroup;
+  constructor(private apiService: ApiService) {
+  }
+
+  submit() {
+    this.apiService.getApiData().subscribe(data=>{
+      console.log(data);
+      if(!data){
+
+      }
+    });
+  }
 }
