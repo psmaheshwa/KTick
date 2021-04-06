43cee0ed5c7c873d3eb5e5b0248f649ffd8a5d49
Avinash Singh <avinash.20399@gmail.com>
2021-04-06 15:28:17 +0530

    Implemented login using msal

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path : '',
-    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
   }
 ];
 
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private msalService: MsalService) { }
 
   ngOnInit(): void {
+    this.msalService.instance.handleRedirectPromise().then(
+      res => {
+        if(res != null && res.account != null){
+          this.msalService.instance.setActiveAccount(res.account);
+        }
+      }
+    )
+  }
+
+  isLoggedIn(): boolean{
+    return this.msalService.instance.getActiveAccount() != null
+  }
+
+  
+  login(){
+    this.msalService.loginRedirect();
+  }
+
+  logout(){
+    this.msalService.logout();
   }
 
 }
