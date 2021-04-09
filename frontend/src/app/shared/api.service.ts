27912f95cec6541dff4b2e4cc5e3ee059cd8ce78
Maheshwaran P <maheshwaran.cs17@bitsathy.ac.in>
2021-04-09 15:12:53 +0530

    Get All user Api

--- frontend/src/app/shared/api.service.ts
+++ frontend/src/app/shared/api.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {AuthService} from "./auth.service";
+import {throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +18,22 @@ export class ApiService {
   constructor(private http: HttpClient, private authService: AuthService) {
   }
 
+  getAllUsers(){
+    let url = `${this.baseUri}/users`;
+    return this.http.get(url,{
+      headers:new HttpHeaders().append('Content-Type','application/json').append('Authorization', `Bearer ${this.access_token}`)
+    }).pipe(catchError(this.errorMgmt));
+  }
 
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 
 }
--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -3,8 +3,7 @@ import {MatTableDataSource} from "@angular/material/table";
 import {MatSort} from "@angular/material/sort";
 import {User} from './user';
 import {MatPaginator} from "@angular/material/paginator";
-import {users} from './user'
-
+import {ApiService} from "../shared/api.service";
 
 
 @Component({
@@ -12,21 +11,30 @@ import {users} from './user'
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements AfterViewInit {
+export class UserComponent implements AfterViewInit, OnInit {
+  users: User[];
 
-  constructor() {
+  constructor(private apiService: ApiService) {
   }
 
   displayedColumns: string[] = ['uniqueId', 'name', 'role', 'email'];
-  dataSource = new MatTableDataSource(users);
+  dataSource;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+
+  ngOnInit(): void{
+    this.apiService.getAllUsers().subscribe(response => {
+      this.dataSource = response;
+      this.dataSource.sort = this.sort;
+    })
+  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -34,5 +42,6 @@ export class UserComponent implements AfterViewInit {
       this.dataSource.paginator.firstPage();
     }
   }
+
 
 }
