445efe33af3b6f81260dcd18df8c266fdd831c0f
Maheshwaran P <maheshwaran.cs17@bitsathy.ac.in>
2021-05-07 11:49:18 +0530

    Last 7 days aggregate data for chart

--- api/routes/ticketsRoute.js
+++ api/routes/ticketsRoute.js
@@ -15,7 +15,7 @@ router.route('/totalToday').get(ensureAuthenticated, ticketController.todayTicke
 router.route('/dueExceed').get(ensureAuthenticated, ticketController.dueExceeded);
 router.route('/created').get(ensureAuthenticated, ticketController.createdByMe, ticketController.allTickets);
 router.route('/assigned').get(ensureAuthenticated, ticketController.assignedToMe, ticketController.allTickets);
-
+router.route('/weekChart').get(ensureAuthenticated,ticketController.weeklyData);
 router
     .route('/assignedToMe')
     .get(ensureAuthenticated,ticketController.assigned);
