fe7006168964e79a4ebd7868b93a5cece4999ba1
Avinash Singh <avinash.20399@gmail.com>
2021-05-09 09:51:01 +0530

    Added fulfillment for dialogflow

--- bot-api/server/routers/dialogflow.js
+++ bot-api/server/routers/dialogflow.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const {WebhookClient} = require('dialogflow-fulfillment');
 
+
 // Import Intent handlers
 testingIntentHandler = require('../intentHandlers/testingIntent');
-ticketsAssignedIntentHandler = require('../intentHandlers/ticketAssignedIntent');
+ticketsAssignedIntentHandler = require('../intentHandlers/ticketsAssignedIntent');
+dailyTicketIntentHandler = require('../intentHandlers/dailyTicketIntent');
+dueTicketIntentHandler = require('../intentHandlers/dueTicketsIntent');
+assignedTicketsIntentHandler = require('../intentHandlers/ticketsAssignedIntent');
 
 const router = express.Router();
 
 router.post('/',(req,res)=>{
     // making agent to listen to webhook
     let agent = new WebhookClient({request:req,response:res});
-    console.log(req.body.originalDetectIntentRequest.payload);
- 
+
     let IntentMap = new Map(); 
 
     // Intent Handlers
+    IntentMap.set('AssignedTicketsIntent',assignedTicketsIntentHandler);
+    IntentMap.set('DueTicketsIntent',dueTicketIntentHandler);
+    IntentMap.set('TodayTicketsIntent',dailyTicketIntentHandler);
     IntentMap.set('TestingIntent',testingIntentHandler);
     IntentMap.set('ShowTicketsAssignedToMe',ticketsAssignedIntentHandler);
 
